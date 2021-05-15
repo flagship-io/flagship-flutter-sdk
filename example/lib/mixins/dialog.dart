c5f12e66344126d43742f3bfb7f130eb996bb8a5
guigithub <guillaume.jacquart@abtasty.com>
2021-05-15 08:38:10 +0200

    Add missing file

---
+++ example/lib/mixins/dialog.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+mixin ShowDialog<T extends StatefulWidget> on State<T> {
+  showBasicDialog(String titleMsg, String? subTitle) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return new AlertDialog(
+            title: new Text(titleMsg),
+            content: new Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: subTitle == null
+                  ? <Widget>[]
+                  : <Widget>[
+                      Text(subTitle),
+                    ],
+            ),
+            actions: <Widget>[
+              new TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Close'),
+              ),
+            ],
+          );
+        });
+  }
+}
