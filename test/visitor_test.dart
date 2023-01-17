ffe3d686b084871589cf8891de2707531997fe28
adel <adel@abtasty.com>
2023-01-17 11:29:48 +0100

    fix CI

--- test/visitor_test.dart
+++ test/visitor_test.dart
@@ -8,7 +8,10 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'test_tools.dart';
+
 void main() {
+  ToolsTest.sqfliteTestInit();
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences.setMockInitialValues({});
   var v1 = Visitor(
