093fde5658c424327a39f47b50ae205955b2d538
Adel <adel@abtasty.com>
2023-09-28 12:38:55 +0200

    fix unit test for missing imp in path provider

--- test/fake_path_provider_platform.dart
+++ test/fake_path_provider_platform.dart
@@ -1,5 +1,3 @@
-// Inspired from https://github.com/flutter/packages/blob/main/packages/path_provider/path_provider/test/path_provider_test.dart
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
